d43d5aecfd8b563fddc11f3c3e411597f9a41c45
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-03 14:19:59 +0300

    Task dao fix!

--- app/src/main/java/com/mercan/roomdatabasemvvm/data/database/TaskDao.kt
+++ app/src/main/java/com/mercan/roomdatabasemvvm/data/database/TaskDao.kt
@@ -1,5 +1,6 @@
 package com.mercan.roomdatabasemvvm.data.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -19,5 +20,5 @@ interface TaskDao {
     suspend fun delete(task: Task)
 
     @Query("SELECT * FROM tasks WHERE isCompleted = :isCompleted ORDER BY dueDate ASC")
-    fun getTasks(isCompleted: Boolean)
+    fun getTasks(isCompleted: Boolean): LiveData<List<Task>>
 }
