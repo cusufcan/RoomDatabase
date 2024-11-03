d47bdfb5f2955b8bae9bb76662d9cf6c754caf6e
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-03 16:41:14 +0300

    enableEdgeToEdge added to MainActivity!

--- app/src/main/java/com/mercan/roomdatabasemvvm/ui/main/MainActivity.kt
+++ app/src/main/java/com/mercan/roomdatabasemvvm/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mercan.roomdatabasemvvm.ui.main
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         bind()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
