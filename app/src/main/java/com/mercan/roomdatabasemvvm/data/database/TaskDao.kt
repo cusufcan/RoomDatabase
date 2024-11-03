ae62f8934acec48a7d2016d56db555603493e1ac
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-03 14:19:29 +0300

    Task dao completed!

---
+++ app/src/main/java/com/mercan/roomdatabasemvvm/data/database/TaskDao.kt
@@ -0,0 +1,23 @@
+package com.mercan.roomdatabasemvvm.data.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.mercan.roomdatabasemvvm.data.model.Task
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Query("SELECT * FROM tasks WHERE isCompleted = :isCompleted ORDER BY dueDate ASC")
+    fun getTasks(isCompleted: Boolean)
+}
