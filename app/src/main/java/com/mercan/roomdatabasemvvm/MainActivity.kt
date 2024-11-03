8f9b75d9ebe8846f9acfb731504dca1c394a4bbb
Yusuf Can Mercan <cusufcan@gmail.com>
2024-11-03 14:16:24 +0300

    ViewBinding to MainActivity!

--- app/src/main/java/com/mercan/roomdatabasemvvm/MainActivity.kt
+++ app/src/main/java/com/mercan/roomdatabasemvvm/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.mercan.roomdatabasemvvm
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mercan.roomdatabasemvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        bind()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun bind() {
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
     }
 }
